1f7d32c02fdf6e083dcf0d7adad68010e0ff3f16
LAPTOP_LENNARD\Lennard <lennardeijsackers92@gmail.com>
2017-11-01 20:47:11 +0100

    setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module",
+    },
+    "rules":{
+        "linebreak-style": 0
+    }
+};
--- prompter.js
+++ prompter.js
@@ -3,47 +3,59 @@ const {promisify} = require('util');
 
 prompt.message = '';
 prompt.delimeter = '';
-// define a prompter
+/**
+ * Prompter wraps the prompt library with an async call to retrieve userInput,
+ * returning an object containing that object
+ */
 class Prompter {
-    
+    /**
+     * @constructor
+     * @param {prompt} prompter - implementation of the prompt in nodejs,
+     *  uses prompt library
+     * @param {schema} schema - schema object definining a set
+     * of properties to require with the prompt} prompter
+     */
     constructor(prompter, schema) {
         this.prompter = prompter;
         this._get = promisify(prompter.get);
         this.schema = schema;
     }
 
+    /**
+     * async wrapper around the prompt library
+     * uses promisify to turn prompt into promise
+     * @return {object}
+     */
     async userInput() {
         this.prompter.start();
 
         let result = await this._get(this.schema).then((result) => {
             return result;
         }).catch((err) => console.log(err));
-        
         return result;
     }
 }
 
-//define the schema
+// define the schema
 const schema = {
     properties: {
         email: {
+            // eslint-disable-next-line
             pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             message: 'Provide your mendix developer account',
-            required: true
+            required: true,
         },
         apiKey: {
             message: 'Provide your API key',
-            required: true
+            required: true,
         },
         projectName: {
-            default: ''
+            default: '',
         },
         branch: {
-            default: 'trunk'
-        }
-    }
-}
-
-
+            default: 'trunk',
+        },
+    },
+};
 
 module.exports = new Prompter(prompt, schema);
